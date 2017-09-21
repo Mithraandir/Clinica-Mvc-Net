13c3b3e6b87655bb763f12ae66534ba5f42c6db5
javier-altmann <javier.altmann@hotmail.com>
2017-09-21 12:49:39 -0300

    Subo todo el proyecto funcionando

---
+++ DAL/TestDAO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class TestDAO : BaseDAO<Usuario>
+    {
+        private ClinicaNetDBEntities context;
+
+        public TestDAO()
+        {
+            context = new ClinicaNetDBEntities();
+        }
+
+        public List<Usuario> listar()
+        {
+            var listado = context.Usuarios.ToList();
+            return listado;
+        }
+    }
+}
